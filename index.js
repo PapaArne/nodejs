d2b650e0eab363d86a096d59b25e1b8d80a70849
Vermoens Arne <arne.vermoens@combell.group>
2025-07-31 14:02:54 +0200

    Verwijder app.get('/') zodat index.html uit www wordt geserveerd

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Statische bestanden serveren uit 'public' folder
+// Statische bestanden serveren uit 'www' folder
 app.use(express.static('www'));
-
-// Optioneel: root route die een bericht geeft
-app.get('/', (req, res) => {
-  res.send('Hello wereld!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server draait op poort ${PORT}`);
 });
-
